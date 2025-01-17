e631f4d03965608ea672e00bef2c9be6b6f4300a
Dávid Koppány <57870734+kdavid96@users.noreply.github.com>
2025-01-17 18:23:18 +0100

    Update Contact.jsx
    removed captcha because of GDPR

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,7 +3,7 @@ import ReCAPTCHA from 'react-google-recaptcha';
 
 
 function Contact() {
-    const [isVerified, setVerified] = useState(false);
+    const [isVerified, setVerified] = useState(true);
     const [message, setMessage] = useState("");
     const recaptchaRef = React.createRef();
 
@@ -47,14 +47,6 @@ function Contact() {
                     <form className='flex flex-col justify-start align-center p-8 w-full' onSubmit={sendEmail}>
                         <input className='p-2 rounded-md border-2 border-slate-200 mb-4 w-full text-left' type="email" id='email' placeholder='E-mail cím'/>
                         <textarea rows={5} className='p-2 rounded-md border-2 border-slate-200 mb-4 w-full text-left' type="text" id='text' placeholder='Üzenet szövege'/>
-                        <div className='my-0 mx-auto w-fit'>
-                            <ReCAPTCHA
-                                ref={recaptchaRef}
-                                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-                                render="explicit"
-                                onChange={onReCAPTCHAChange}
-                                />
-                        </div>
                         <button className='mt-2 rounded-md bg-lime-400 text-white px-2 py-4' type="submit">Küldés</button>
                     </form>
                     <p className='font-semibold text-center text-red-600'>{message}</p>
